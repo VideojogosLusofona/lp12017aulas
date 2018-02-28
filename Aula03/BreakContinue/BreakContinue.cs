2b4de4f79f2ad5e0239630ef464b077f0d83dcfd
Nuno Fachada <nfachada@laseeb.org>
2018-02-28 00:55:46 +0000

    Adicionar alguns comentários ao projeto BreakContinue

--- Aula03/BreakContinue/BreakContinue.cs
+++ Aula03/BreakContinue/BreakContinue.cs
@@ -12,19 +12,23 @@ namespace Aula03
             {
                 turn++;
 
+                // Ends loop if we're over the maximum number of turns
                 if (turn >= maxTurns)
                 {
                     break;
                 }
 
+                // Skips current cycle if current turn is divisible by 3
                 if (turn % 3 == 0)
                 {
                     continue;
                 }
 
+                // Print turn number
                 Console.WriteLine($"Turn number {turn}");
             }
 
+            // Print number of last turn
             Console.WriteLine($"Last turn was {turn}");
         }
     }
