8484114c76bd8ad3eca22378e4a5b1d82c9b692c
Nuno Fachada <nfachada@laseeb.org>
2018-02-28 00:24:52 +0000

    Adicionar projetos IfExample e BreakContinue para a aula 3

---
+++ Aula03/BreakContinue/BreakContinue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula03
+{
+    class BreakContinue
+    {
+        static void Main(string[] args)
+        {
+            const int maxTurns = 20;
+            int turn = 0;
+            while (true)
+            {
+                turn++;
+
+                if (turn >= maxTurns)
+                {
+                    break;
+                }
+
+                if (turn % 3 == 0)
+                {
+                    continue;
+                }
+
+                Console.WriteLine($"Turn number {turn}");
+            }
+
+            Console.WriteLine($"Last turn was {turn}");
+        }
+    }
+}
---
+++ Aula03/IfExample/IfExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03
+{
+    class IfExample
+    {
+        static void Main(string[] args)
+        {
+            char smile = '\u263A';
+            char frown = '\u2639';
+            char skull = '\u2620';
+
+            SpaceShip spaceShip = new SpaceShip();
+
+            bool shieldsUp = spaceShip.Shield > 0;
+            bool armourOk = spaceShip.Armour > 0;
+
+            if (shieldsUp && armourOk)
+            {
+                Console.WriteLine($"You're doing great {smile}");
+            }
+            else if (shieldsUp ^ armourOk)
+            {
+                Console.WriteLine($"You don't look so good {frown}");
+            }
+            else
+            {
+                Console.WriteLine($"You're dead {skull}");
+            }
+        }
+    }
+}
