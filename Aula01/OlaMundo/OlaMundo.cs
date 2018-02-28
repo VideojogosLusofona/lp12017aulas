c0146f2f05fa2da23af4da8d825cc4dd21dc9e16
Nuno Fachada <nfachada@laseeb.org>
2018-02-28 14:42:10 +0000

    Adicionar projetos para aula 1

---
+++ Aula01/OlaMundo/OlaMundo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula01
+{
+    class OlaMundo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");
+            Console.ReadKey();
+        }
+    }
+}
