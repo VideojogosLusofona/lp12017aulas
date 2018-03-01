04b38a72d1fced398658dce0f9d7fc3da942ee8f
Nuno Fachada <nfachada@laseeb.org>
2018-03-01 11:11:16 +0000

    Adicionar programa sobre flag enums (aula 3)

---
+++ Aula03/WoWRoles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // I have Tank and Damage roles
+            WoWRoles myRoles = WoWRoles.Tank | WoWRoles.Damage;
+
+            // Am I a healer?
+            if ((myRoles & WoWRoles.Healer) == WoWRoles.Healer)
+            {
+                Console.WriteLine("I heal you!");
+            }
+            else
+            {
+                Console.WriteLine("I DON'T heal you!");
+            }
+
+            // Show my current roles
+            Console.WriteLine(myRoles);
+
+            // Deactivate Tank role
+            myRoles &= ~WoWRoles.Tank;
+
+            // Toggle (switch) Healer role
+            myRoles ^= WoWRoles.Healer;
+
+            // Show my current roles
+            Console.WriteLine(myRoles);
+
+        }
+    }
+}
---
+++ Aula03/WoWRoles/WoWRoles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula03
+{
+    [Flags]
+    public enum WoWRoles
+    {
+        Tank   = 1 << 0, // 1
+        Healer = 1 << 1, // 2
+        Damage = 1 << 2  // 4
+    }
+}
