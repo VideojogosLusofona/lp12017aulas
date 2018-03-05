ab45825be1cc43362d8f83bcb177c2c55435dc6e
Nuno Fachada <nfachada@laseeb.org>
2018-03-05 14:43:45 +0000

    Adicionar resolução do Exercicio 2/Aula 3 (NPCPerks)

---
+++ Aula03/NPCPerks/NPCPerks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03
+{
+    [Flags]
+    enum NPCPerks
+    {
+        Stealth = 1 << 0,
+        Combat = 1 << 1,
+        Lockpick = 1 << 2,
+        Luck = 1 << 3
+    }
+}
---
+++ Aula03/NPCPerks/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Aula03
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Constantes
+            const NPCPerks niceCombo = NPCPerks.Combat | NPCPerks.Luck;
+
+            // Variaveis
+            NPCPerks[] npcs;
+            string tmp;
+            int numNPCs;
+
+            // Pedir numero de PCs ao utilizador
+            Console.Write("Indica o numero de NPCs: ");
+            tmp = Console.ReadLine();
+            numNPCs = Convert.ToInt32(tmp);
+
+            // Criar array de NPCs
+            npcs = new NPCPerks[numNPCs];
+            npcs.Initialize();
+
+            // Pedir ao utilizador para indicar as caracteristicas de cada NPC
+            for (int i = 0; i < numNPCs; i++)
+            {
+                // Inicializar NPC atual a zero (sem nada ainda)
+                npcs[i] = 0;
+
+                Console.WriteLine("NPC " + (i + 1) + "\n------------");
+                // Este NPC tem característica Stealth?
+                do
+                {
+                    Console.Write("Stealth (s/n)? ");
+                    tmp = Console.ReadLine();
+                } while ((tmp != "s") && (tmp != "n"));
+
+                if (tmp == "s")
+                {
+                    npcs[i] |= NPCPerks.Stealth;
+                }
+
+                // Este NPC tem característica Combat?
+                do
+                {
+                    Console.Write("Combat (s/n)? ");
+                    tmp = Console.ReadLine();
+                } while ((tmp != "s") && (tmp != "n"));
+
+                if (tmp == "s")
+                {
+                    npcs[i] |= NPCPerks.Combat;
+                }
+
+                // Este NPC tem característica Lockpick?
+                do
+                {
+                    Console.Write("Lockpick (s/n)? ");
+                    tmp = Console.ReadLine();
+                } while ((tmp != "s") && (tmp != "n"));
+
+                if (tmp == "s")
+                {
+                    npcs[i] |= NPCPerks.Lockpick;
+                }
+
+                // Este NPC tem característica Luck?
+                do
+                {
+                    Console.Write("Luck(s/n)? ");
+                    tmp = Console.ReadLine();
+                } while ((tmp != "s") && (tmp != "n"));
+
+                if (tmp == "s")
+                {
+                    npcs[i] |= NPCPerks.Luck;
+                }
+            }
+
+            // Mostrar caracteristicas de cada NPC
+            Console.WriteLine("\nCaracteristicas de cada NPC\n---------------");
+            for (int i = 0; i < numNPCs; i++)
+            {
+                Console.Write($"NPC {i + 1}: {npcs[i]}");
+                if ((npcs[i] & niceCombo) == niceCombo)
+                {
+                    Console.Write("\t<<< You shall win all fights! >>>");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
