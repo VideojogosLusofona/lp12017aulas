7cfdd6b304c286a3a6c5e6f0b0e0c5ccce9ed91f
Nuno Fachada <nfachada@laseeb.org>
2018-03-16 16:17:47 +0000

    Adicionar exercícios da aula 5

---
+++ Aula05/Exercicio4/Exercicio5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula05
+{
+    class Exercicio5
+    {
+        static void Main(string[] args)
+        {
+            int nDados, soma = 0;
+            Random r = new Random();
+
+            // Pedir número de dados ao utilizador
+            Console.Write("Quantos dados queres lançar? ");
+            nDados = Convert.ToInt32(Console.ReadLine());
+
+            // Lançar dados o número de vezes indicado pelo utilizador
+            for (int i = 0; i < nDados; i++)
+            {
+                // Obter valor do dado entre 1 e 6
+                int valorDado = r.Next(1, 7);
+
+                // Somar valor do dado ao total até agora
+                soma += valorDado;
+
+                // Mostrar valor do dado (não era pedido, mas ajuda a perceber)
+                Console.WriteLine($"- Valor dado {i + 1}: {valorDado}");
+            }
+
+            // Mostrar soma de todos os lançamentos de dados
+            Console.WriteLine("=> Total: " + soma);
+        }
+    }
+}
---
+++ Aula05/InstanciasDiferentes/InstanciasDiferentes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula05
+{
+    class InstanciasDiferentes
+    {
+        const int seed = 22;
+        const int maxValue = 100;
+        const int numNumbers = 4;
+
+        static void Main(string[] args)
+        {
+            Random r1 = new Random(seed);
+            Random r2 = new Random(seed);
+
+            // Mostrar números aleatórios com o 1o gerador
+            for (int i = 0; i < numNumbers; i++)
+                Console.WriteLine(r1.Next(maxValue));
+
+            Console.WriteLine();
+
+            // Mostrar numeros aleatorios com o 2o gerador
+            for (int i = 0; i < numNumbers; i++)
+                Console.WriteLine(r2.Next(maxValue));
+        }
+    }
+}
