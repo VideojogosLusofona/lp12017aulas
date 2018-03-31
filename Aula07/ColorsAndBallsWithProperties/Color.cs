2f9a3f42a368a3a302e0e49a95a47002b11be65b
Nuno Fachada <nfachada@laseeb.org>
2018-03-31 20:44:52 +0100

    Adicionar exercício de criação de objetos para a aula 7

---
+++ Aula07/ColorsAndBallsWithProperties/Color.cs
@@ -0,0 +1,37 @@
+﻿namespace Aula07
+{
+    class Color
+    {
+        // Auto-implemented properties
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte Alpha { get; set; }
+
+        // Greyscale property - does not need support variable
+        public byte Greyscale
+        {
+            get
+            {
+                return (byte)((Red + Green + Blue) / 3);
+            }
+        }
+
+        // Constructors
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+
+        public Color(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = 255;
+        }
+    }
+}
