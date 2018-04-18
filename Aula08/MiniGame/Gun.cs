f82e7a19e8177b012b28b039330c0885dbebe19b
Nuno Fachada <nfachada@laseeb.org>
2018-04-18 23:25:23 +0100

    Adicionar problema MiniGame para a aula 8

---
+++ Aula08/MiniGame/Gun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula08
+{
+    public class Gun : Weapon
+    {
+        public int Ammo { get; private set; }
+
+        public Gun(float power, int ammo) : base(power)
+        {
+            Ammo = ammo;
+        }
+
+        public void FireGun()
+        {
+            Ammo--;
+            Console.WriteLine($"\tFiring gun with power={power}, {Ammo} bullets left");
+        }
+    }
+}
